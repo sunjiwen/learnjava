5b3618ce3e137a7bb806c95793d11e368dd04f8b
sunjiwen <sunjiwen@192.168.0.173>
2017-08-26 12:32:03 +0800

    some wrong

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hi this is github!");
-		System.out.println("Hi this is from Mac!”);
+		System.out.println("Hi this is from Mac!");
                 System.out.println("Hi this is from ubunt!");
 	}
 }
